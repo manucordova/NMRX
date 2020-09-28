6ae43ffadbad81d52e5bf067e86f8595d4df36f9
manucordova <manuel.cordova@epfl.ch>
2020-09-28 18:09:19 +0200

    Different optimization schemes, submit scripts updated

---
+++ run/submit_Helvetios.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --mem 185G
+#SBATCH --ntasks-per-node=36
+#SBATCH --cpus-per-task=1
+#SBATCH -A lrm
+
+module purge
+module load gcc openblas/0.3.6-openmp
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+set +e
+
+conda activate SMLp3
+
+f=$1
+f=${f#run_}
+f=${f%.py}
+
+k=$2
+
+for i in `seq 1 36`
+do
+  srun -n 1 --exclusive --mem=5G python $1 $(( $i + $k )) > output_${f}_$(( $i + $k )).log &
+  sleep 5
+done
+wait
